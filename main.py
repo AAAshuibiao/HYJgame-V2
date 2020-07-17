5a92739badf0cbeb75ca1ba5c1f237fbdf63fec9
AAA_shuibiao <aaashuibiao@gmail.com>
2020-07-17 02:58:47 -0400

    ***fixed all bugs***

--- main.py
+++ main.py
@@ -8,30 +8,40 @@ pygame.init()
 
 screen = pygame.display.set_mode((500, 500), 0, 32)
 
+
+"""
 class color_:
     def __init__(self):
         r = random.randint(0,255)
         g = random.randint(0,255)
         b = random.randint(0,255)
-        self.clr = (r,g,b) 
+        self.clr = (r,g,b)
+"""
+
 
 def returnColor():
-    r,g,b = random.randint(0,256), random.randint(0,256), random.randint(0,256)
+    r,g,b = random.randint(0,255), random.randint(0,255), random.randint(0,255)
+    return (r, g, b)
 
 
 try:
-
     while True:
 
         for event in pygame.event.get():
             if event.type == QUIT:
-                pygame.display.quit()
+                sys.exit()
                 print("No u")
 
-        screen.fill(color_A.clr)
+
+        c = returnColor()
+
+        print(c)
+
+        screen.fill(c)
 
         x, y = pygame.mouse.get_pos()
 
+        pygame.draw.rect(screen, returnColor(), (x-25, y-25, 50, 50))
 
         pygame.display.update()
 
@@ -40,11 +50,7 @@ except SystemExit:
     pass
 
 
-
-
-
 #class block:
 #    def __init__(self):
 #        pygame.draw.rect()
 
-        
