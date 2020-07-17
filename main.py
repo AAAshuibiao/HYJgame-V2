696c8144033849697bd0c895c262df648bc8e8ff
AAA_shuibiao <aaashuibiao@gmail.com>
2020-07-17 03:47:38 -0400

    finalized the first game

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import sys
 import random
-import math
 import pygame
 from pygame.locals import *
 
 pygame.init()
+
+print("Game by SS and ZW: press the mouse button when color inside the square is closest to the rest")
+pygame.time.wait(1000)
 
 screen = pygame.display.set_mode((500, 500), 0, 32)
 
@@ -17,6 +19,8 @@ class color_:
         b = random.randint(0,255)
         self.clr = (r,g,b)
 """
+
+gayClock = pygame.time.Clock()
 
 
 def returnColor():
@@ -33,11 +37,14 @@ try:
         for event in pygame.event.get():
             if event.type == QUIT:
                 sys.exit()
-                print("No u")
+
             if event.type == MOUSEBUTTONDOWN:
                 mousePressed = True
-                totalDifference = abs(c[0]-d[0])/20
-                print("You're score is" + totalDifference )
+                totalDifference = abs(c[0]-d[0]) + abs(c[1]-d[1]) + abs(c[2]-d[2])
+                score = 100 - (totalDifference // 5)
+                print("Color of the background " + str(c) )
+                print("Color of the square: " + str(d) )
+                print("Your score is " + str(score) + " points out of 100" )
 
         if not mousePressed:
             i += 1
@@ -50,11 +57,13 @@ try:
         
 
         pygame.draw.rect(screen, d, (x-25, y-25, 50, 50))
-        if i > 1500:
+        if i > 30:
             i = 0
             c = returnColor()
             d = returnColor()
         pygame.display.update()
+
+        gayClock.tick(60)
         
         
 
